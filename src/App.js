38b40cea8ea75a76d00d5c5d21dc5f38561b0579
Saathi <saathibys@gmail.com>
2022-02-06 12:17:00 +0530

    added alanBtn and its api key

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ import { useEffect } from 'react';
 
 function App() {
 
+  const alanKey = 'ea90a5be89e9a7ae5653b41d0ecf3efa2e956eca572e1d8b807a3e2338fdd0dc/stage';
+
   useEffect(() => {
-    
+    alanBtn({
+      key: alanKey,
+    })
   }, [])
 
   return (
